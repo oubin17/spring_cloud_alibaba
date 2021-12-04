3cca4e3d869b63400ed83e92532c51d74e05e63b
oubin <oubin17@gmail.com>
2021-12-04 12:15:28 +0800

    loadBalancerClient

--- order/src/main/java/com/oubin/spring_cloud_alibaba/order/configuration/RestConfiguration.java
+++ order/src/main/java/com/oubin/spring_cloud_alibaba/order/configuration/RestConfiguration.java
@@ -1,5 +1,6 @@
 package com.oubin.spring_cloud_alibaba.order.configuration;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -14,6 +15,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestConfiguration {
 
+    @LoadBalanced
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
--- order/src/main/java/com/oubin/spring_cloud_alibaba/order/service/OrderService.java
+++ order/src/main/java/com/oubin/spring_cloud_alibaba/order/service/OrderService.java
@@ -4,6 +4,7 @@ import com.oubin.spring_cloud_alibaba.order.configuration.LoadBalance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -20,21 +21,68 @@ import java.util.List;
 @RestController
 public class OrderService {
 
-    @Autowired
     private DiscoveryClient discoveryClient;
 
-    @Autowired
     private RestTemplate restTemplate;
 
-    @Autowired
     private LoadBalance loadBalance;
+
+    private LoadBalancerClient loadBalancerClient;
+
+    /**
+     * 本地自定义负载均衡
+     *
+     * @return
+     */
     @GetMapping("/order")
-    public Object orderToMember() {
+    public String orderToMember() {
 
         List<ServiceInstance> instances = discoveryClient.getInstances("member");
         ServiceInstance balance = loadBalance.balance(instances);
         String result = restTemplate.getForObject(balance.getUri() + "/user", String.class);
         return "从nacos获取到的生产者地址:" + balance.getUri() + " 获取到的userName: " + result;
 
+    }
+
+    /**
+     * ribbon负载均衡
+     *
+     * @return
+     */
+    @GetMapping("order/ribbon")
+    public String ribbonOrder() {
+        String result = restTemplate.getForObject("http://member" + "/user", String.class);
+        return "获取到的userName: " + result;
+    }
+
+    /**
+     * loadbalancerClient实现负载均衡
+     *
+     * @return
+     */
+    @GetMapping("order/client")
+    public String clientOrder() {
+        ServiceInstance member = loadBalancerClient.choose("member");
+        return "从nacos获取到的生产者地址:" + member.getUri();
+    }
+
+    @Autowired
+    public void setDiscoveryClient(DiscoveryClient discoveryClient) {
+        this.discoveryClient = discoveryClient;
+    }
+
+    @Autowired
+    public void setRestTemplate(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @Autowired
+    public void setLoadBalance(LoadBalance loadBalance) {
+        this.loadBalance = loadBalance;
+    }
+
+    @Autowired
+    public void setLoadBalancerClient(LoadBalancerClient loadBalancerClient) {
+        this.loadBalancerClient = loadBalancerClient;
     }
 }
