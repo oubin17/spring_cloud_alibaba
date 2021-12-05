963ea9540ca9fa11ca91c9828a203f5d7558e021
oubin <oubin17@gmail.com>
2021-12-05 18:21:56 +0800

    Gateway

---
+++ gateway/src/main/java/com/oubin/spring_cloud_alibaba/gateway/GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.oubin.spring_cloud_alibaba.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
---
+++ gateway/src/test/java/com/oubin/spring_cloud_alibaba/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oubin.spring_cloud_alibaba.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
